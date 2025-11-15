97d0e109bbe8b8ab191199023c59c37dab1d9e2e
Ariel Massetti <ariel@finconecta.com>
2025-11-15 21:07:29 +0000

    FE - Adding pages in Dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -7,6 +7,31 @@ const Dashboard = () => {
     <div>
       <h2>Dashboard</h2>
       <p>Welcome, {user?.username}!</p>
+      <p>You are login in as {user?.role === "ROLE_ADMIN" ? "Administrator" : "Teacher"}</p>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            Register Student
+          </div>
+          <div className="col">
+            Register Teacher
+          </div>
+          <div className="col">
+            Register Course
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            Register Subject
+          </div>
+          <div className="col">
+            Enroll student
+          </div>
+          <div className="col">
+            Reports
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
